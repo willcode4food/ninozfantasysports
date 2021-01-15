1a1996cace728d3c48f24e320689a0212e04ef9b
Marc Arbesman <marbesman@gmail.com>
2021-01-15 08:12:58 -0500

    adding testing library for form tests

---
+++ src/components/AccountSecurityForm/index.test.js
@@ -0,0 +1,22 @@
+// import WithSession from 'providers/WithSession'
+// import { render, cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom'
+// import AccountSecurityForm from './'
+
+// let useContextMock
+// Setup mock
+// beforeEach(() => {
+//     useContextMock = React.useContext = jest.fn()
+// })
+
+// afterEach(cleanup)
+
+describe('AccountSecurityForm', () => {
+    // useContextMock = React.useContext = jest.fn()
+    // useContextMock.mockReturnValue({ email: 'hsimpson@springfieldpower.com', uid: '234234234' })
+    // it('should render the basic fields', () => {
+    //     render(<AccountSecurityForm />)
+    //     expect(0).toBe(0)
+    //     // expect(screen.getByRole('heading', { name: 'Email and Password' })).toBeInTheDocument()
+    // })
+})
