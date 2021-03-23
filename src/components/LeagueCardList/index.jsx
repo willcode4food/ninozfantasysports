204afeb5dbafe5ee53681cde90a83e865bcdf7ee
Marc Arbesman <marbesman@gmail.com>
2021-03-22 21:22:56 -0400

    fixes league list sorting

--- src/components/LeagueCardList/index.jsx
+++ src/components/LeagueCardList/index.jsx
@@ -1,49 +1,36 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import LeagueCard from 'components/LeagueCard'
-import { StaticQuery, graphql } from 'gatsby'
+import { useStaticQuery, graphql } from 'gatsby'
 import { LeagueListWrapper } from './styles'
 import { leagueSeasonSorter } from 'utils/arrayHelpers'
 
 function LeagueCardList() {
-    return (
-        <StaticQuery
-            query={graphql`
-                query AllLeagues {
-                    ninozFantasySports {
-                        returnAllLeagues {
-                            id
-                            name
-                            description
-                            seasons {
-                                id
-                                name
-                                startDate
-                                endDate
-                            }
-                            type
-                            ownerId
-                            ownerName
-                        }
+    const data = useStaticQuery(graphql`
+        query AllLeagues {
+            ninozFantasySports {
+                returnAllLeagues {
+                    id
+                    name
+                    description
+                    seasons {
+                        id
+                        name
+                        startDate
+                        endDate
                     }
+                    type
+                    ownerId
+                    ownerName
                 }
-            `}
-            render={(data) => {
-                const [sortedLeagues, setSortedLeagues] = useState(data.ninozFantasySports.returnAllLeagues)
-                useEffect(() => {
-                    const leaguesSortedBySeason = data.ninozFantasySports.returnAllLeagues.sort(
-                        leagueSeasonSorter('seasons', 'startDate')
-                    )
-                    setSortedLeagues(leaguesSortedBySeason)
-                }, [data])
-                return (
-                    <LeagueListWrapper>
-                        {sortedLeagues.map((league) => (
-                            <LeagueCard league={league} key={league.id} />
-                        ))}
-                    </LeagueListWrapper>
-                )
-            }}
-        />
+            }
+        }
+    `)
+    return (
+        <LeagueListWrapper>
+            {data.ninozFantasySports.returnAllLeagues.sort(leagueSeasonSorter('seasons', 'startDate')).map((league) => (
+                <LeagueCard league={league} key={league.id} />
+            ))}
+        </LeagueListWrapper>
     )
 }
 
